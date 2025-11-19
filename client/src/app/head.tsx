26d87ff4742cebf89ac755b7384347f03a3ffe06
Antoni Seba <antoniwoj@o2.pl>
2025-11-19 07:53:26 +0100

    sdfs

---
+++ client/src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" type="image/png" href="/logo.png" />
+      <link rel="apple-touch-icon" type="image/png" href="/logo.png" />
+    </>
+  )
+}
+
